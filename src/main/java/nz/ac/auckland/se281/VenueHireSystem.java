b2868de9c2d88a641370e1b7e19630c74d2cfc0c
Harry <hma481@aucklanduni.ac.nz>
2024-04-07 01:39:27 +1300

    makeBooking method now prints message if systemdate is not set

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -143,7 +143,10 @@ public class VenueHireSystem {
   }
 
   public void makeBooking(String[] options) {
-    // TODO implement this method
+    if (systemDate == null) {
+      MessageCli.BOOKING_NOT_MADE_DATE_NOT_SET.printMessage();
+      return;
+    }
   }
 
   public void printBookings(String venueCode) {
